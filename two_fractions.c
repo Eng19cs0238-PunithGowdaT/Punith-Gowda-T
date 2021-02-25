b7414c4b393be8922a1d8af11c4acbf9a5dd0e5c
Eng19cs0238-PunithGowdaT <76142497+Eng19cs0238-PunithGowdaT@users.noreply.github.com>
2021-02-25 16:45:27 +0530

    Update two_fractions.c

--- two_fractions.c
+++ two_fractions.c
@@ -1,6 +1,4 @@
-//WAP to find the sum of two fractions.
 #include<stdio.h>
-#include<conio.h>
 typedef struct
 {
   int n;
@@ -8,30 +6,51 @@ typedef struct
 
 } Fraction;
 
+int
+simplified (int x, int y)
+{
+    if (y != 0)
+        return simplified(y, x % y);
+    else
+        return x;
+};
+
 Fraction
 sum (Fraction f1, Fraction f2)
 {
-  Fraction result = { (f1.n * f2.d) + (f2.n * f1.d), f1.d * f2.d };
-  return result;
+  Fraction c;
+  int x, y;
+  x = (f1.n * f2.d) + (f2.n * f1.d);
+  y = (f1.d * f2.d);
+  printf("%d",simplified(x,y));
+  c.n = x / simplified (x, y);
+  c.d = y / simplified (x, y);
+  return c;
 }
 
+
+
 Fraction
-input (Fraction c, int a)
+input (int a)
 {
+  Fraction c;
   printf ("Enter fraction %d: numerator and denominator:", a);
   scanf ("%d%d", &c.n, &c.d);
   return c;
 };
 
-void display(Fraction r){
-  printf ("Result=%d/%d", r.n, r.d);
+void
+display (Fraction f1, Fraction f2, Fraction r)
+{
+  printf ("Result of %d/%d + %d/%d = %d/%d", f1.n, f1.d, f2.n, f2.d,r.n, r.d);
 }
+
 int
 main ()
 {
-  Fraction f1 = input (f1, 1);
-  Fraction f2 = input (f2, 2);
+  Fraction f1 = input (1);
+  Fraction f2 = input (2);
   Fraction r = sum (f1, f2);
-  display(r);
-  return 0; 
+  display (f1, f2, r);
+  return 0;
 }
